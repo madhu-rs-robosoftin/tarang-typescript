cee35b813c73d6c09fea937e4515aceb0c890db1
madhu-rs-robosoftin <72455173+madhu-rs-robosoftin@users.noreply.github.com>
2020-10-08 16:19:47 +0530

    Refactored app folder to accurately use webpack config

---
+++ app/components/index.js
@@ -0,0 +1,26 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Loadable from 'react-loadable';
+// import { BrowserRouter } from 'react-router-dom';
+
+
+// import App from './routes';
+// import "./normalizestyles.css"
+// import ErrorBoundary from './shared/components/errorboundary'
+
+
+// Loadable.preloadReady().then(() => {
+// 	ReactDOM.render(
+// 			<BrowserRouter>
+// 				<React.Fragment>
+// 					<ErrorBoundary>
+// 						<App />
+// 					</ErrorBoundary>
+// 				</React.Fragment>
+// 			</BrowserRouter>,
+// 		document.getElementById('root')
+// 	);
+// });
+
+
+export default App;
