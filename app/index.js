a89d5a9787fe8fbb2ed52df9d9f342b7f1780591
madhu-rs-robosoftin <72455173+madhu-rs-robosoftin@users.noreply.github.com>
2020-10-08 14:39:22 +0530

    modified app folder

--- app/index.js
+++ app/index.js
@@ -1,30 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Loadable from 'react-loadable';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 
 import App from './routes';
 import configureStore from './store';
-import GlobalStyle from './GlobalStyle';
-import ErrorBoundary from './/ErrorBoundary';
+import "./normalizestyles.css"
+import ErrorBoundary from './shared/components/errorboundary'
 
 
 // Initial State can be passed here
 const store = configureStore(preloadedState); 
 Loadable.preloadReady().then(() => {
 	ReactDOM.render(
-		<Provider store={store}>
 			<BrowserRouter>
 				<React.Fragment>
-					<GlobalStyle />
 					<ErrorBoundary>
 						<App />
 					</ErrorBoundary>
 				</React.Fragment>
-			</BrowserRouter>
-		</Provider>,
+			</BrowserRouter>,
 		document.getElementById('root')
 	);
 });
