ed7f0eb1b7ff8f16e5e7a6ab3c4f280c38b99c9b
madhu-rs-robosoftin <72455173+madhu-rs-robosoftin@users.noreply.github.com>
2020-10-13 19:15:42 +0530

    revert config in eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,120 +1,132 @@
-const fs = require('fs');
+/* eslint-disable */
 const path = require('path');
+const fs = require('fs');
+
+/* eslint-enable */
 
 const prettierOptions = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
 );
 
-module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
-    ecmaFeatures: {
-      jsx: true // Allows for the parsing of JSX
-    }
-  },
-  settings: {
-    react: {
-      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
-    }
-  },
-  extends: [
-    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-  ],
-  rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-  },
-};
-
-// module.exports = {
-//   parser: '@typescript-eslint/parser',
-//   extends: [
-//     "plugin:@typescript-eslint/recommended",
-//     "plugin:react/recommended",
-//     "prettier/@typescript-eslint",
-//     "plugin:prettier/recommended"
-//   ],
-//   env: {
-//     jest: true,
-//     browser: true,
-//     node: true,
-//     es6: true,
+// export const parser = '@typescript-eslint/parser';
+// export const parserOptions = {
+//   ecmaVersion: 2020,
+//   sourceType: 'module',
+//   ecmaFeatures: {
+//     jsx: true,
 //   },
-//   parserOptions: {
-//     ecmaVersion: 6,
-//     sourceType: 'module',
-//     ecmaFeatures: {
-//       jsx: true,
+// };
+// export const settings = {
+//   react: {
+//     version: 'detect',
+//   },
+//   'import/resolver': {
+//     webpack: {
+//       config: './internals/webpack/webpack.prod.babel.js',
 //     },
-//   },
-//   rules: {
-//     'prettier/prettier': ['error', prettierOptions],
-//     'arrow-body-style': [2, 'as-needed'],
-//     'class-methods-use-this': 0,
-//     'import/imports-first': 0,
-//     'import/newline-after-import': 0,
-//     'import/no-dynamic-require': 0,
-//     'import/no-extraneous-dependencies': 0,
-//     'import/no-named-as-default': 0,
-//     'import/no-unresolved': 2,
-//     'import/no-webpack-loader-syntax': 0,
-//     'import/prefer-default-export': 0,
-//     indent: [
-//       2,
-//       2,
-//       {
-//         SwitchCase: 1,
-//       },
-//     ],
-//     'jsx-a11y/aria-props': 2,
-//     'jsx-a11y/heading-has-content': 0,
-//     'jsx-a11y/label-has-associated-control': [
-//       2,
-//       {
-//         // NOTE: If this error triggers, either disable it or add
-//         // your custom components, labels and attributes via these options
-//         // See https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-associated-control.md
-//         controlComponents: ['Input'],
-//       },
-//     ],
-//     'jsx-a11y/label-has-for': 0,
-//     'jsx-a11y/mouse-events-have-key-events': 2,
-//     'jsx-a11y/role-has-required-aria-props': 2,
-//     'jsx-a11y/role-supports-aria-props': 2,
-//     'max-len': 0,
-//     'newline-per-chained-call': 0,
-//     'no-confusing-arrow': 0,
-//     'no-console': 1,
-//     'no-unused-vars': 2,
-//     'no-use-before-define': 0,
-//     'prefer-template': 2,
-//     'react/destructuring-assignment': 0,
-//     'react-hooks/rules-of-hooks': 'error',
-//     'react/jsx-closing-tag-location': 0,
-//     'react/forbid-prop-types': 0,
-//     'react/jsx-first-prop-new-line': [2, 'multiline'],
-//     'react/jsx-filename-extension': 0,
-//     'react/jsx-no-target-blank': 0,
-//     'react/jsx-uses-vars': 2,
-//     'react/require-default-props': 0,
-//     'react/require-extension': 0,
-//     'react/self-closing-comp': 0,
-//     'react/sort-comp': 0,
-//     'require-yield': 0,
-//   },
-//   settings: {
-//     'import/resolver': {
-//       webpack: {
-//         config: './internals/webpack/webpack.prod.babel.js',
-//       },
-//       node : {
-//         "extensions" : ['.js','.jsx','.ts','.tsx']
-//       },
+//     node: {
+//       extensions: ['.js', '.jsx', '.ts', '.tsx'],
 //     },
 //   },
 // };
+// export const extends = [
+//   'plugin:react/recommended',
+//   'plugin:@typescript-eslint/recommended',
+//   'prettier/@typescript-eslint',
+//   'plugin:prettier/recommended',
+// ];
+// export const rules = {
+//   // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+//   // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+// };
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    jest: true,
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+    'arrow-body-style': [2, 'as-needed'],
+    'class-methods-use-this': 0,
+    'import/imports-first': 0,
+    'import/newline-after-import': 0,
+    'import/no-dynamic-require': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default': 0,
+    'import/no-unresolved': 2,
+    'import/no-webpack-loader-syntax': 0,
+    'import/prefer-default-export': 0,
+    indent: [
+      2,
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'jsx-a11y/aria-props': 2,
+    'jsx-a11y/heading-has-content': 0,
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        // NOTE: If this error triggers, either disable it or add
+        // your custom components, labels and attributes via these options
+        // See https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-associated-control.md
+        controlComponents: ['Input'],
+      },
+    ],
+    'jsx-a11y/label-has-for': 0,
+    'jsx-a11y/mouse-events-have-key-events': 2,
+    'jsx-a11y/role-has-required-aria-props': 2,
+    'jsx-a11y/role-supports-aria-props': 2,
+    'max-len': 0,
+    'newline-per-chained-call': 0,
+    'no-confusing-arrow': 0,
+    'no-console': 1,
+    'no-unused-vars': 2,
+    'no-use-before-define': 0,
+    'prefer-template': 2,
+    'react/destructuring-assignment': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-closing-tag-location': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-first-prop-new-line': [2, 'multiline'],
+    'react/jsx-filename-extension': 0,
+    'react/jsx-no-target-blank': 0,
+    'react/jsx-uses-vars': 2,
+    'react/require-default-props': 0,
+    'react/require-extension': 0,
+    'react/self-closing-comp': 0,
+    'react/sort-comp': 0,
+    'require-yield': 0,
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: './internals/webpack/webpack.prod.babel.js',
+      },
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+};
